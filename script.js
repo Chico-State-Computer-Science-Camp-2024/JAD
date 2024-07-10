fce4582fbc07fe893e0269c54ae2b647ce132944
Javier Jacobo <173719689+JavierE-J@users.noreply.github.com>
2024-07-10 21:24:37 +0000

    Fixed some bugs

--- script.js
+++ script.js
@@ -1,43 +1,3 @@
-<<<<<<< HEAD
-// function openCity(evt, cityName) {
-//     // Declare all variables
-//     var i, tabcontent, tablinks;
-  
-//     // Get all elements with class="tabcontent" and hide them
-//     tabcontent = document.getElementsByClassName("tabcontent");
-//     for (i = 0; i < tabcontent.length; i++) {
-//       tabcontent[i].style.display = "none";
-//     }
-  
-//     // Get all elements with class="tablinks" and remove the class "active"
-//     tablinks = document.getElementsByClassName("tablinks");
-//     for (i = 0; i < tablinks.length; i++) {
-//       tablinks[i].className = tablinks[i].className.replace(" active", "");
-//     }
-  
-//     // Show the current tab, and add an "active" class to the button that opened the tab
-//     document.getElementById(cityName).style.display = "block";
-//     evt.currentTarget.className += " active";
-//   }
-let angle = 0;
-let lense;
-
-function preload(){
-  lense = loadModel('Lense.obj');
-}
-function setup(){
-  createCanvas(100,100,WEBGL);
-  
-}
-function draw(){
-  background(120, 120,120);
-  rotateX(angle);
-  rotateY(angle);
-  rotateZ(angle);
-  model(lense);
-  angle += 0.03;
-}
-=======
 
   function openPage(pageName,elmnt,color) {
     var i, tabcontent, tablinks;
@@ -54,4 +14,3 @@ function draw(){
   }
   
   document.getElementById("defaultOpen").click();
->>>>>>> d32b587f858dea751432dcb4ced0aa20ee15dff3
